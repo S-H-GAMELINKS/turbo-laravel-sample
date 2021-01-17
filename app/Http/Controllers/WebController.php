4e95cc4668d5aa99b7cfc453f17fec0c04230fa0
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-17 18:14:04 +0900

    add index() & about()

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    //
+    public function index() {
+        return view('web.index');
+    }
+
+    public function about() {
+        return view('web.about');
+    }
 }
