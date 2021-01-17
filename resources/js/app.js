cd3b591bba398c01bb61f1a1c5856ae47c93d684
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-17 18:07:07 +0900

    php artisan turbo:install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import {
+    start as startTurbo
+} from '@hotwired/turbo';
+import('./bootstrap');
+import('./elements/turbo-echo-stream-tag');
+import { Application } from 'stimulus'
+import { definitionsFromContext } from '@stimulus/webpack-helpers'
+import 'alpinejs';
+
+startTurbo();
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
