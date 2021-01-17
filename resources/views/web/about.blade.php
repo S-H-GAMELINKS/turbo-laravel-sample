227d4306fc5528d03c564679eeb7e2d5dd96f06e
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-17 18:40:06 +0900

    add view for turbo-laravel

---
+++ resources/views/web/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<turbo-frame id="index">
+    <h1>Hello, About!</h1>
+    <a href="/">
+        move to Index
+    </a>
+</turbo-frame>
+@endsection
---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<turbo-frame id="index">
+    <h1>Hello, World!</h1>
+    <a href="/about">
+        move to About
+    </a>
+</turbo-frame>
+@endsection
